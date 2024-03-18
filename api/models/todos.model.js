bc85fa2cb2f2a5981c2d27a77e48f018ffbc0502
Rochelle <rochellejoan.p.esteves@gmail.com>
2024-03-18 16:28:57 +0800

    create todo model

---
+++ api/models/todos.model.js
@@ -0,0 +1,18 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timeStamp: true }
+);
+
+
+const Todo = mongoose.model("Todo", todoSchema)
+
+export default Todo;
