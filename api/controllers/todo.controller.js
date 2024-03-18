9da3ebc4ed4e9abfc1b19fdafacfdddcd24346a1
Rochelle <rochellejoan.p.esteves@gmail.com>
2024-03-18 17:46:08 +0800

    add getTodo and getAllTodos api

--- api/controllers/todo.controller.js
+++ api/controllers/todo.controller.js
@@ -1,10 +1,30 @@
-import Todo from "../models/todos.model.js"
+import Todo from "../models/todos.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createTodo = async (req, res, next) => {
+  try {
+    const createTodo = await Todo.create(req.body);
+    return res.status(201).json(createTodo);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTodo = async (req, res, next) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) return next(errorHandler(404, "Todo is not found"));
+    res.status(200).json(todo);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllTodos = async (req, res, next) => {
     try {
-        const createTodo = await Todo.create(req.body)
-        return res.status(201).json(createTodo)
+      const todos = await Todo.find({});
+      res.status(200).json(todos);
     } catch (error) {
-        next(error)
+      next(error);
     }
-}
+  };
--- api/routes/todo.routes.js
+++ api/routes/todo.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createTodo } from "../controllers/todo.controller.js";
+import { createTodo, getAllTodos, getTodo } from "../controllers/todo.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createTodo)
-
+router.get("/getTodo/:id", getTodo);
+router.get("/getTodos", getAllTodos);
 
 export default router;
